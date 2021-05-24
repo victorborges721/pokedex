13ecf4e2bb22073e725db836d242ff309b38971e
Victor Borges <victor.borges.721@gmail.com>
2021-05-24 16:51:42 -0400

    Moved Modal from Card component to new PokemonModal component.

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Button, Col, Container, Modal, Row } from "react-bootstrap";
 import "./style.css";
 import pokemonType from "../../helpers/pokemonTypes";
+import PokemonModal from "../PokemonModal";
 
 const Card = ({ pokemon }) => {
   // manage modal
@@ -18,7 +19,6 @@ const Card = ({ pokemon }) => {
     while (num.length < size) num = "0" + num;
     return num;
   };
-
   const pokeNum = padZeros(pokemon.id, 3);
 
   return (
@@ -43,62 +43,12 @@ const Card = ({ pokemon }) => {
           })}
         </div>
       </div>
-      <Modal
-        show={showModal}
-        onHide={handleClose}
-        animation={false}
-        size="lg"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title className="Card-name">{pokemon.name}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Container>
-            <Row>
-              <Col xs={12} md={5}>
-                <div className="Modal-img">
-                  <img src={spriteUrl} alt={pokemon.name} />
-                </div>
-              </Col>
-              <Col xs={12} md={7}>
-                <div className="Modal-types">
-                  <h5>Type(s):</h5>
-                  {pokemon.types.map((type) => {
-                    return (
-                      <div
-                        className="Modal-type"
-                        style={{ background: pokemonType[type.type.name] }}
-                      >
-                        {type.type.name}
-                      </div>
-                    );
-                  })}
-                </div>
-                <div className="Modal-types">
-                  <h5>Weaknesses:</h5>
-                  {pokemon.types.map((type) => {
-                    return (
-                      <div
-                        className="Modal-type"
-                        style={{ background: pokemonType[type.type.name] }}
-                      >
-                        {type.type.name}
-                      </div>
-                    );
-                  })}
-                </div>
-              </Col>
-            </Row>
-            <Row></Row>
-          </Container>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="danger" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      <PokemonModal
+        pokemon={pokemon}
+        showModal={showModal}
+        handleClose={handleClose}
+        spriteUrl={spriteUrl}
+      />
     </>
   );
 };
--- src/components/PokemonModal/PokemonModal.js
+++ src/components/PokemonModal/PokemonModal.js
@@ -1,7 +1,69 @@
 import React from "react";
+import { Button, Col, Container, Modal, Row } from "react-bootstrap";
+import pokemonType from "../../helpers/pokemonTypes";
+import "./style.css";
 
-const PokemonModal = () => {
-  return <div></div>;
+const PokemonModal = ({ pokemon, showModal, handleClose, spriteUrl }) => {
+  return (
+    <>
+      <Modal
+        show={showModal}
+        onHide={handleClose}
+        animation={false}
+        size="lg"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title className="Card-name">{pokemon.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Container>
+            <Row>
+              <Col xs={12} md={5}>
+                <div className="Modal-img">
+                  <img src={spriteUrl} alt={pokemon.name} />
+                </div>
+              </Col>
+              <Col xs={12} md={7}>
+                <div className="Modal-types">
+                  <h5>Type(s):</h5>
+                  {pokemon.types.map((type) => {
+                    return (
+                      <div
+                        className="Modal-type"
+                        style={{ background: pokemonType[type.type.name] }}
+                      >
+                        {type.type.name}
+                      </div>
+                    );
+                  })}
+                </div>
+                <div className="Modal-types">
+                  <h5>Weaknesses:</h5>
+                  {pokemon.types.map((type) => {
+                    return (
+                      <div
+                        className="Modal-type"
+                        style={{ background: pokemonType[type.type.name] }}
+                      >
+                        {type.type.name}
+                      </div>
+                    );
+                  })}
+                </div>
+              </Col>
+            </Row>
+            <Row></Row>
+          </Container>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 };
 
 export default PokemonModal;
