91ec70b81f4bb7f6ba1b70bb7ca2ba63a1baad62
Victor Borges <victor.borges.721@gmail.com>
2021-06-07 10:38:11 -0400

    Cleaned up unused code in components.

--- src/helpers/pokeCalcs.js
+++ src/helpers/pokeCalcs.js
@@ -144,22 +144,12 @@ export const getEvolutions = (evoData, evoChain) => {
 
     evoChain.push({
       species_name: evoData.species.name,
-      // min_level: !evoData ? 1 : evoData.min_level,
-      // trigger_name: !evoData ? null : evoData.trigger.name,
-      // item: !evoData ? null : evoData.item,
     });
 
     if (numberOfEvolutions > 1) {
       for (let i = 1; i < numberOfEvolutions; i++) {
         evoChain.push({
           species_name: evoData.evolves_to[i].species.name,
-          // min_level: !evoData.evolves_to[i]
-          //   ? 1
-          //   : evoData.evolves_to[i].min_level,
-          // trigger_name: !evoData.evolves_to[i]
-          //   ? null
-          //   : evoData.evolves_to[i].trigger.name,
-          // item: !evoData.evolves_to[i] ? null : evoData.evolves_to[i].item,
         });
       }
     }
